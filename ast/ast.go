6b232835a80f6416dee2b6846e00c02d4d18df91
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-05 21:39:23 +0900

    Add return statement

--- ast/ast.go
+++ ast/ast.go
@@ -36,9 +36,19 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
 }
 
 type Identifier struct {
