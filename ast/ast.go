a23ff2d089d03fa7d6eca6f8f9549b6399da6a46
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-07 21:44:28 +0900

    Add operator precedence parser

--- ast/ast.go
+++ ast/ast.go
@@ -175,7 +175,8 @@ func (oe *InfixExpression) String() string {
 	var out bytes.Buffer
 
 	out.WriteString("(")
-	out.WriteString(oe.Operator)
+	out.WriteString(oe.Left.String())
+	out.WriteString(" " + oe.Operator + " ")
 	out.WriteString(oe.Right.String())
 	out.WriteString(")")
 
