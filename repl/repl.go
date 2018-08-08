c4a9de619dd3138f0be785110afa6cd65d1a0284
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-08 20:41:02 +0900

    Update REPL to calling parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,18 @@ import (
 	"io"
 
 	"github.com/wasanx25/gopter/lexer"
-	"github.com/wasanx25/gopter/token"
+	"github.com/wasanx25/gopter/parser"
 )
 
 const PROMPT = ">> "
+
+const ERROR_AREARE = `
+.　　∧∧　∧∧　ｱﾚｱﾚ?
+　∩ﾟДﾟ,≡,ﾟДﾟ)
+　 ｀ヽ　 　 |）
+　　　 | _　|～
+　　 　U U
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +31,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ERROR_AREARE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
