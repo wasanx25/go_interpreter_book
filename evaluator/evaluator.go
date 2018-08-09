62a4e5eb9583a62537dab60d48981cf3736cb569
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-10 00:42:47 +0900

    Add prefix bang evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -21,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -42,4 +45,26 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	}
 
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case FALSE:
+		return TRUE
+	case TRUE:
+		return FALSE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
