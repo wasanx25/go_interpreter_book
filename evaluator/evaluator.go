1a2ec2723686b7553261fdcc679588e28ae94159
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-10 00:42:18 +0900

    Fix use private method

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,7 +20,7 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
