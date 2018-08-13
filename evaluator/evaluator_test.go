950d368a53930d4fe22cd1e5a89f6407f981d4b7
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-13 22:55:12 +0900

    Add string concatenation evaluator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -166,6 +166,10 @@ func TestErrorHandling(t *testing.T) {
 		{
 			"foobar", "identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -256,6 +260,20 @@ func TestClosures(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
