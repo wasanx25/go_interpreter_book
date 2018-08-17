020710a167bd8232d140863c9dd95d78811b164b
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-18 02:03:35 +0900

    Add hash literal parser

--- parser/parser.go
+++ parser/parser.go
@@ -87,6 +87,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
+	p.registerPrefix(token.LBRACE, p.parseHashLiteral)
+
 	p.nextToken()
 	p.nextToken()
 
@@ -486,4 +488,33 @@ func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
 	}
 
 	return exp
+}
+
+func (p *Parser) parseHashLiteral() ast.Expression {
+	hash := &ast.HashLiteral{Token: p.curToken}
+	hash.Pairs = make(map[ast.Expression]ast.Expression)
+
+	for !p.peekTokenIs(token.RBRACE) {
+		p.nextToken()
+		key := p.parseExpression(LOWEST)
+
+		if !p.expectPeek(token.COLON) {
+			return nil
+		}
+
+		p.nextToken()
+		value := p.parseExpression(LOWEST)
+
+		hash.Pairs[key] = value
+
+		if !p.peekTokenIs(token.RBRACE) && !p.expectPeek(token.COMMA) {
+			return nil
+		}
+	}
+
+	if !p.expectPeek(token.RBRACE) {
+		return nil
+	}
+
+	return hash
 }
