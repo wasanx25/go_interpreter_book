1baacc01b8a4492ca2c9f163742ba4da3a5e3eed
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-08-22 00:44:19 +0900

    Add expand macro evaluator

--- evaluator/macro_expansion.go
+++ evaluator/macro_expansion.go
@@ -24,6 +24,32 @@ func DefineMacro(program *ast.Program, env *object.Environment) {
 	}
 }
 
+func ExpandMacros(program ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		callExpression, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+
+		macro, ok := isMacroCall(callExpression, env)
+		if !ok {
+			return node
+		}
+
+		args := quoteArgs(callExpression)
+		evalEnv := extendMacroEnv(macro, args)
+
+		evaluated := Eval(macro.Body, evalEnv)
+
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			panic("we only support returning AST-nodes from macros")
+		}
+
+		return quote.Node
+	})
+}
+
 func isMacroDefinition(node ast.Statement) bool {
 	letStatement, ok := node.(*ast.LetStatement)
 	if !ok {
@@ -48,4 +74,43 @@ func addMacro(stmt ast.Statement, env *object.Environment) {
 		Body:       macroLiteral.Body,
 	}
 	env.Set(letStatement.Name.Value, macro)
+}
+
+func isMacroCall(exp *ast.CallExpression, env *object.Environment) (*object.Macro, bool) {
+	identifier, ok := exp.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(identifier.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		return nil, false
+	}
+
+	return macro, true
+}
+
+func quoteArgs(exp *ast.CallExpression) []*object.Quote {
+	args := []*object.Quote{}
+
+	for _, a := range exp.Arguments {
+		args = append(args, &object.Quote{Node: a})
+	}
+
+	return args
+}
+
+func extendMacroEnv(macro *object.Macro, args []*object.Quote) *object.Environment {
+	extended := object.NewEnclosedEnvironment(macro.Env)
+
+	for paramIdx, param := range macro.Parameters {
+		extended.Set(param.Value, args[paramIdx])
+	}
+
+	return extended
 }
